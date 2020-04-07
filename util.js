959b4fce6e4f269b4f30476b8783791e9f479f79
YUKIJUDAI <1787149082@qq.com>
2020-04-07 10:41:53 +0800

    init

--- util.js
+++ util.js
@@ -1,4 +1,71 @@
 export default {
+    /********* dom *********/
+
+    //添加事件
+    addEvent(obj, type, fn) {
+        if (obj.addEventListener) {
+            obj.addEventListener(type, fn, false);
+        } else if (obj.attachEvent) {//IE
+            obj.attchEvent('on' + type, fn);
+        }
+    }
+    //移除事件
+    removeEvent(obj, type, fn) {
+        if (obj.removeEventListener) {
+            obj.removeEventListener(type, fn, false);
+        } else if (obj.detachEvent) {//兼容IE
+            obj.detachEvent('on' + type, fn);
+        }
+    }
+    //阻止默认行为
+    preDef(ev) {
+        var e = ev || window.event;
+        if (e.preventDefault) {
+            e.preventDefault();
+        } else {
+            e.returnValue = false;
+        }
+    }
+    //获取目标对象
+    getTarget(ev) {
+        if (ev.target) {//w3c
+            return ev.target;
+        } else if (window.event.srcElement) {//IE
+            return window.event.srcElement;
+        }
+    }
+    //获取滚动条位置
+    getSP() {
+        return {
+            top: document.documentElement.scrollTop || document.body.scrollTop,
+            left: document.documentElement.scrollLeft || document.body.scrollLeft;
+        }
+    }
+    //获取可视窗口大小
+    getWindow() {
+        if (typeof window.innerWidth != 'undefined') {
+            return {
+                width: window.innerWidth,
+                height: window.innerHeight
+            }
+        } else {
+            return {
+                width: document.documentElement.clientWidth,
+                height: document.documentElement.clientHeight
+            }
+        }
+    }
+    // 获取鼠标坐标
+    mousePosition(ev) {
+        if (ev.pageX || ev.pageY) {
+            return { x: ev.pageX, y: ev.pageY };
+        }
+        return {
+            x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
+            y: ev.clientY + document.body.scrollTop - document.body.clientTop
+        };
+    }
+
     /********* Browser *********/
 
     // 判断是否是电脑端
@@ -48,5 +115,20 @@ export default {
             if (arr[i] === item) return true;
         }
         return false;
+    }
+
+    /********* 正则 ************/
+
+    // 千分位分隔
+    cc(s) {
+        if (/[^0-9\.]/.test(s)) return "invalid value";
+        s = s.replace(/^(\d*)$/, "$1.");
+        s = (s + "00").replace(/(\d*\.\d\d)\d*/, "$1");
+        s = s.replace(".", ",");
+        var re = /(\d)(\d{3},)/;
+        while (re.test(s))
+            s = s.replace(re, "$1,$2");
+        s = s.replace(/,(\d\d)$/, ".$1");
+        return s.replace(/^\./, "0.")
     }
 };
