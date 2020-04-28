1a0a0a2c409500ce0f04a546f8dd9c7d337a9861
YUKIJUDAI <1787149082@qq.com>
2020-04-28 15:41:41 +0800

    init

--- 打卡.js
+++ 打卡.js
@@ -1,64 +1,78 @@
 //一分钟内产生随机数打卡
-var randomTime = Math.random()*10*1000;
-console.log('打卡计时开始'+(randomTime/1000).toFixed(2)+'秒后打卡');
-toast('打卡计时开始'+(randomTime/1000).toFixed(2)+'秒后打卡');
-setTimeout(()=>{
-    console.log('开始打卡');
-    unlock();
-    punchClock();
-    console.log('打卡结束');
-}, randomTime);
+console.log('20秒后开始运行程序');
+setTimeout(() => {
+    // checkTime();
+    punchClock("下班打卡");
+}, 20000);
 
-//解锁
-function unlock(){
-    //息屏和锁屏状态需要解锁
-    if(!device.isScreenOn() || desc("快捷方式").exists()){
-        //曲线解锁 miui锁屏滑动不能唤出密码输入 通过下拉通知栏点击时间进入密码解锁
-        device.wakeUp();
-        //下拉状态栏
-        swipe(500, 30, 500, 1000, 300);
-        sleep(400);
-        //点击时间
-        click(100, 120);
-        //解锁 密码5566
-        desc(5).findOne().click();
-        desc(5).findOne().click();
-        desc(6).findOne().click();
-        desc(6).findOne().click();
-        //等待解锁完成
-        text('闹钟').waitFor();
-        //返回主页
-        // home();
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function checkTime() {
+    var now = new Date();
+    var now_Hours = now.getHours();
+    var now_Minutes = now.getMinutes();
+    var now_day = now.getDay();
+
+    if (now_day < 6) {
+        console.log("工作日");
+        console.log("当前系统时间：" + now_Hours + '分' + now_Minutes + '秒');
+        if (now_Hours === 8 && now_Minutes >= 40) {
+            var random = getRandomNumber(0, 10);
+            console.log(1000 * 60 * rangdom + "秒后打上班卡");
+            sleep(1000 * 60 * rangdom);
+            punchClock("上班打卡");
+        } else if (now_Hours === 18 && now_Minutes <= 20) {
+            var random = getRandomNumber(0, 10);
+            console.log(1000 * 60 * rangdom + "秒后打下班卡");
+            sleep(1000 * 60 * rangdom);
+            punchClock("下班打卡");
+        } else {
+            console.log("5分钟后重试")
+            sleep(1000 * 60 * 5)
+            checkIsNow();
+        }
+    } else {
+        console.log("非工作日");
+        sleep(1000 * 60 * 60);
+        checkIsNow();
     }
 }
+
 //打卡
-function punchClock(){
-    sleep(200);
-    //启动钉钉
-    launch("com.alibaba.android.rimet");
-    //等待钉钉启动
-    desc("工作").waitFor();
-    desc("工作").findOne().click();
-    if(!text("考勤打卡").exists()){
-        swipe(500, 1300, 500, 500, 300);
-        sleep(400);
+function punchClock(descStr) {
+    if (!device.isScreenOn() || desc("快捷方式").exists()) {
+        device.wakeUp();
     }
-    text("考勤打卡").findOne().parent().click();
+    sleep(1000);
+    //启动钉钉
+    launchApp("钉钉");
+    //等待钉钉启动
+    desc("工作台").waitFor();
+    desc("工作台").findOne().click();
+    sleep(4000);
+    if (!desc("考勤打卡").exists()) {
+        swipe(500, 1300, 500, 500, 300);
+        sleep(1000);
+    }
+    desc("考勤打卡").findOne().click();
     //等待打卡界面加载
     descContains("班时间").waitFor();
     //打卡按钮名字
-    var descStr = getOpr();
-    if(!desc(descStr).exists()){
+    if (!desc(descStr).exists()) {
         toast(descStr + '不存在');
-        exit()
+        sleep(1000 * 60 * 60);
+        checkTime();
+        return;
     }
-    toast('按钮名:'+ descStr);
+    toast('按钮名:' + descStr);
     var button = desc(descStr).findOne();
     var bounds = button.bounds();
     var x = bounds.centerX();
     var y = bounds.centerY();
     //遮挡滚动
-    if(y > 1776){
+    if (y > 1776) {
         swipe(500, 1500, 500, 300, 300);
         sleep(500);
         button = desc(descStr).findOne();
@@ -66,41 +80,9 @@ function punchClock(){
         x = bounds.centerX();
         y = bounds.centerY();
     }
-    toast('坐标:'+ x +', '+ y);
+    toast('坐标:' + x + ', ' + y);
     //通过点击位置打卡
     click(x, y);
-}
-//根据上下班时间获取打卡按钮名字
-function getOpr(){
-    var span = descContains("班时间").find();
-    var timeArr = [];
-    var now = new Date();
-    var yearStr = (now.getFullYear())+"/"+(now.getMonth()+1)+"/"+(now.getDate())+' ';
-    span.forEach(v=>{
-        var str = v.desc();
-        var exec = /(.*)时间(.*)/.exec(str);
-        var opr = exec[1];
-        var timeStr = exec[2];
-        var opTime = new Date(yearStr + timeStr);
-        //上班时间提前十分钟就可以打卡
-        if(opr == '上班'){
-            opTime.setMinutes(opTime.getMinutes() - 10);
-        }
-        console.log(yearStr + timeStr)
-        timeArr.push({
-            timeStr:timeStr,
-            opr:opr,
-            time:opTime
-        })
-    })
-    var descStr;
-    console.log(timeArr)
-    timeArr.some((v, i)=>{
-        console.log(v.timeStr)
-        if(now > v.time && (i == timeArr.length - 1 || now < timeArr[i+1].time)){
-            descStr = v.opr + '打卡';
-            return true
-        }
-    })
-    return descStr
+    sleep(1000 * 60 * 60);
+    checkTime();
 }
